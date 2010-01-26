4f9a6aeb0e48a1f8352a1ad7fce9caa61e1a0784
Bruce Sheplan <brucesheplan@Bruce-Sheplans-MacBook-Pro.local>
2010-01-26 01:21:05 -0400

    -- Added '-n' option to limit output to n theaters -- Added stored procedure that cleans up the repeated movies -- Changed time format to 24hrs for easier sorting -- Added usage message -- Added mysql escapes for the ' character -- Added '-c' option to create schema & stored proc if missing

--- cinescraper.py
+++ cinescraper.py
@@ -50,17 +50,48 @@ CREATE TABLE IF NOT EXISTS `schedule_hour` (
   `schedule_id` int(11) default NULL,
   PRIMARY KEY  (`id`)
 ) ENGINE=InnoDB DEFAULT CHARSET=utf8;
+
+delimiter //
+
+DROP PROCEDURE IF EXISTS cleanup_movies//
+
+CREATE PROCEDURE cleanup_movies()
+BEGIN
+
+SELECT @num_repeats := SUM(1)  FROM (SELECT *, SUM(1) as count FROM movie GROUP by name ORDER BY id) as movies WHERE movies.count > 1 INTO @null;
+
+repeat_label: LOOP
+
+SELECT id FROM (SELECT @id := id as id, @name := name FROM (SELECT *, SUM(1) as count FROM movie GROUP by name ORDER BY id) as movies WHERE count > 1 LIMIT 1) as temp1 INTO @null;
+UPDATE schedule SET movie_id = @id WHERE movie_id IN (SELECT id FROM movie WHERE name = @name);
+DELETE FROM movie WHERE name = @name AND id <> @id;
+
+SELECT @num_repeats := SUM(1)  FROM (SELECT *, SUM(1) as count FROM movie GROUP by name ORDER BY id) as movies WHERE movies.count > 1 INTO @null;
+
+IF @num_repeats > 0 THEN ITERATE repeat_label; END IF;
+
+LEAVE repeat_label;
+END LOOP repeat_label;
+
+END//
+
+delimiter ;
+
 """
 
 def main(argv):
 	
 	try:
-		opts, args = getopt.getopt(argv, "tsc", ["text", "sql", "create"])
+		opts, args = getopt.getopt(argv, "tscn:h", ["text", "sql", "create"])
 	except getopt.GetoptError:
-		print "Did not understand arguments\n"
+		print "ERROR: Did not understand arguments\n"
+		usage()
 		sys.exit(2)
 	
 	output = "sql"
+	max_theaters = 0
+	create = 0
+	
 	for opt, arg in opts:
 		if opt in ("-t", "--text"):
 			output = "text"
@@ -68,19 +99,27 @@ def main(argv):
 			output = "sql"
 		elif opt in ("-c", "--create"):
 			create = 1
+		elif opt in ("-n"):
+			max_theaters = int(arg)
+		elif opt in ("-h"):
+			usage()
+			sys.exit(0)
+			
 	
-	allData = getAllData()
+	allData = getAllData(max_theaters)
+	
+	sql = "DELETE FROM movie; DELETE FROM schedule; DELETE FROM schedule_hour; DELETE FROM theater;"
 	
 	for t in allData:
 		text = "********** " + t['theaterName'] + " **********\n"
 		
-		sql = "INSERT INTO theater (name) VALUES ('" + t['theaterName'] + "');\n"
+		sql += "INSERT INTO theater (name) VALUES ('" + t['theaterName'].replace('\'','\\\'') + "');\n"
 		sql += "SET @theater_id := LAST_INSERT_ID();\n"
 				
 		for x in t['data']:
 			text += x['movieName'] + "\n"
 			
-			sql += "INSERT INTO movie (name) VALUES ('" + x['movieName'] + "');\n"
+			sql += "INSERT INTO movie (name) VALUES ('" + x['movieName'].replace('\'','\\\'') + "');\n"
 			sql += "SET @movie_id := LAST_INSERT_ID();\n"
 			sql += "INSERT INTO schedule (movie_id, theater_id) VALUES (@movie_id,@theater_id);\n"
 			sql += "SET @schedule_id := LAST_INSERT_ID();\n"
@@ -90,20 +129,22 @@ def main(argv):
 			text += 'MON-FRI: ' + x['showtimes'][0]	+ "\n"
 			for time in p.split(x['showtimes'][0]):
 				sql += "INSERT INTO schedule_hour (schedule_id, day_M, day_T, day_W, day_Th, day_F, day_Sa, day_Su, time) "
-				sql += " VALUES (@schedule_id,'1','1','1','1','1','0','0','" + time.strip() + "');\n"
+				sql += " VALUES (@schedule_id,'1','1','1','1','1','0','0','" + timeTo24Hrs(time.strip()) + "');\n"
 			
 			text += 'SAT: ' + x['showtimes'][1]	+ "\n"
 			for time in p.split(x['showtimes'][1]):
 				sql += "INSERT INTO schedule_hour (schedule_id, day_M, day_T, day_W, day_Th, day_F, day_Sa, day_Su, time) "
-				sql += " VALUES (@schedule_id,'0','0','0','0','0','1','0','" + time.strip() + "');\n"
+				sql += " VALUES (@schedule_id,'0','0','0','0','0','1','0','" + timeTo24Hrs(time.strip()) + "');\n"
 
 			text += 'SUN & HOL: ' + x['showtimes'][2]	+ "\n"
 			for time in p.split(x['showtimes'][2]):
 				sql += "INSERT INTO schedule_hour (schedule_id, day_M, day_T, day_W, day_Th, day_F, day_Sa, day_Su, time) "
-				sql += " VALUES (@schedule_id,'0','0','0','0','0','0','1','" + time.strip() + "');\n"
+				sql += " VALUES (@schedule_id,'0','0','0','0','0','0','1','" + timeTo24Hrs(time.strip()) + "');\n"
 
 			text += "=============================\n"
 		text += "\n\n\n"
+		
+		sql += "CALL cleanup_movies;\n"
 		
 		if output == 'sql':
 			if create == 1:
@@ -112,10 +153,42 @@ def main(argv):
 			
 		else:
 			print text.encode('ascii', 'xmlcharrefreplace')
-		
 
-def getAllData():
-	theaters = getTheaterList()
+def usage():
+	print "usage: " + sys.argv[0] + " [-s|--sql] [-t|--text] [-c|--create] [-n limit]\n"
+	print "Options:"
+	print "\t-c, --create:\tOutput schema"
+	print "\t-s, --sql:\tOutput in SQL for piping to mysql"
+	print "\t-t, --text:\tOutput in human readable"
+	print "\t-n limit:\tOutput from n theaters only"
+	print "\t-h:\t\tThis message"
+
+def timeTo24Hrs(time12):
+	stripped_time = time12.strip()
+	
+	split_hour = re.compile(r':')
+	split_ampm = re.compile(r' ')
+	
+	hour_min = split_hour.split(stripped_time)
+	min_ampm = split_ampm.split(hour_min[1])
+	
+	hour = hour_min[0]
+	minute = min_ampm[0]
+	ampm = min_ampm[1]
+	
+	
+	if ampm.upper() == 'AM':
+		ret = (hour + minute).rjust(4,"0")
+	else:
+		ret = str(int(hour) + 12) + minute
+	
+	# print stripped_time + " ==> " + ret
+	
+	return ret
+	
+	
+def getAllData(max_theaters):
+	theaters = getTheaterList(max_theaters)
 	result = []
 	for t in theaters:
 		result.append({'theaterName': t['name'], 'data':getTheaterData(t['url'])})
@@ -146,7 +219,7 @@ def getTheaterData(url):
 	return result
 
 
-def getTheaterList():
+def getTheaterList(max_theaters):
 	res = getPage(baseUrl)
 
 	soup = BeautifulSoup(res)
@@ -161,7 +234,11 @@ def getTheaterList():
 	for t in tNames:
 		theaters.append({'name': t, 'url': baseUrl + '/' + tUrls[i]})
 		i = i+1
-	
+		
+		if max_theaters > 0:
+			if i >= max_theaters:
+				break
+
 	return theaters
 
 
