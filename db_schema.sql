2f21389691db484014a01966c10d73c6bc4e8d0d
Giovanni Collazo <gio@24veces.com>
2010-04-20 02:01:35 -0400

    Added a file with new db schema

---
+++ db_schema.sql
@@ -0,0 +1,30 @@
+﻿# Dump of table movies
+# ------------------------------------------------------------
+
+DROP TABLE IF EXISTS `movies`;
+
+CREATE TABLE `movies` (
+  `id` int(11) NOT NULL auto_increment,
+  `theater_id` int(11) default NULL,
+  `name` text,
+  `period1` text,
+  `period2` text,
+  `period3` text,
+  `period4` text,
+  PRIMARY KEY  (`id`),
+  KEY `theater_id` (`theater_id`),
+  CONSTRAINT `movies_ibfk_1` FOREIGN KEY (`theater_id`) REFERENCES `theaters` (`id`)
+) ENGINE=InnoDB DEFAULT CHARSET=utf8;
+
+
+
+# Dump of table theaters
+# ------------------------------------------------------------
+
+DROP TABLE IF EXISTS `theaters`;
+
+CREATE TABLE `theaters` (
+  `id` int(11) NOT NULL auto_increment,
+  `name` varchar(255) default NULL,
+  PRIMARY KEY  (`id`)
+) ENGINE=InnoDB DEFAULT CHARSET=utf8;
